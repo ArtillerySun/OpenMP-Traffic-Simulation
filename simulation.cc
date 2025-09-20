8203bed38ed13621df0ed9bf6cebe452db6f416d
wxy <auferstanden1@gmail.com>
2025-09-20 17:37:34 +0800

    scrap modular implementation

--- simulation.cc
+++ simulation.cc
@@ -12,139 +12,96 @@
 namespace traffic_prng {
 extern PRNG* engine;
 }
+    // on the same lane, find the position of the immediately next car
+static inline int find_next(std::vector<int> lane, int L, int position) {
+        // min 1 car next
+    for (int i = 1; i < L; i++) {
+            // mod to wrap back 
+        int wrapped_shifted = (position + i) % L;
+        if (lane[wrapped_shifted] != -1) {
+            return wrapped_shifted;
+        } 
+    }
+    return -1; // not supposed to happen
+}
+static inline int find_prev(std::vector<int> lane, int L, int position) {
+    for (int i = 1; i < L; i++) {
+        int wrapped_shifted = (position - i + L) % L;
+        if (lane[wrapped_shifted] != -1) {
+            return wrapped_shifted;
+        } 
+    }
+    return -1; // not supposed to happen
+}
+    // compute distance from 2 position on circular road
+static inline int dist(int L, int prev, int next) { 
+    int d = (next - prev + L) % L; 
+    return d == 0 ? L : d; 
+}
+
+bool can_switch_lane(std::vector<Car>& cars, const Car& c, std::vector<std::vector<int>>& lanes, int L) {
+    int c2 = find_next(lanes[c.lane], L, c.position);
+    int c3 = find_next(lanes[c.lane^1], L, c.position);
+    int c0 = find_prev(lanes[c.lane^1], L, c.position);
+    int d2 = dist(L, c2, c.position);
+    int d0 = dist(L, c.position, c0);
+    int d3 = dist(L, c3, c.position);
+    return d2 < d3 && c.v >= d2 && lanes[c.lane^1][c.position] == -1 && d0 > cars[lanes[c.lane^1][c0]].v;
+}
 
 void executeSimulation(Params params, std::vector<Car> cars) {
-  int n = params.n;
-  int timesteps = params.steps;
-  int t = 0;
-  int vmax = params.vmax;
-  double p_start = params.p_start;
-  double p_dec = params.p_dec;
-  while (t < timesteps) {
-    
-    for (int i = 0; i < n; i++) {
+    const int N = params.n;
+    const int L = params.L;
+    const int T = params.steps;
+    const int VMAX = params.vmax;
+    const double P_START = params.p_start;
+    const double P_DEC = params.p_dec;
+    int t = 0;
+        // force accelerate state 
+    std::vector<char> force_acc(L, 0);
+        // can change lane state
+    std::vector<char> change_lane(L, 0);
+        // lanes and car position states
+    std::vector<std::vector<int>> lanes(2, std::vector(L, -1)); 
+        // centralized per iteration random decision making
+    std::vector<char> ss(N, 0);
+    std::vector<char> dec(N, 0);
 
+        // build position state vectors
+    for (auto c : cars) {
+        lanes[c.lane][c.position] = c.id;
     }
-  }
-}
 
-int c2_id(const std::vector<Car>& cars, const Car& c_, int N) {
-    int best_id = -1;
-    int best_dist = N + 1;
+    #ifdef DEBUG
+    reportResult(cars, 0);
+    #endif
 
-    for (auto const& c : cars) {
-        if (c.lane != c_.lane || c.id == c_.id) continue;
-
-        int dist = (c.position - c_.position + N) % N;
-        if (dist > 0 && dist < best_dist) {
-            best_dist = dist;
-            best_id = c.id;
+    while (t < T) {
+            // Induce randomness
+            // need to skip partition size * idx when omp this to ensure deterministic rng to car correspondence
+        for (int i = 0; i < N; i++) {
+            ss[i] = flip_coin(P_START, traffic_prng::engine);
+            dec[i] = flip_coin(P_DEC, traffic_prng::engine);
+        }
+            // clear change lane decision state
+        std::fill(change_lane.begin(), change_lane.end(), 0);
+            // update lane change decision state
+        for (auto c : cars) {
+            if (can_switch_lane(cars, c, lanes, L)) {
+                change_lane[c.id] = 1;
+            }
+        }
+            // apply change lane 
+        for (int i = 0; i < N; i++) {
+            if (change_lane[i]) {
+                Car& c = cars[i];
+                lanes[c.lane][c.position] = -1;
+                lanes[c.lane^1][c.position] = c.id;
+                c.lane ^= 1;
+            } else {
+                continue;
+            }
         }
     }
-    return best_id;
 }
 
-int c0_id(const std::vector<Car>& cars, const Car& c_, int N) {
-    int best_id = -1;
-    int best_dist = N + 1;
-
-    for (auto const& c : cars) {
-        if (c.lane == c_.lane || c.id == c_.id) continue;
-
-        int dist = (c_.position - c.position + N) % N;
-        if (dist > 0 && dist < best_dist) {
-            best_dist = dist;
-            best_id = c.id;
-        }
-    }
-    return best_id;
-}
-
-
-int c3_id(const std::vector<Car>& cars, const Car& c_, int N) {
-    int best_id = -1;
-    int best_dist = N + 1;
-
-    for (auto const& c : cars) {
-        if (c.lane == c_.lane || c.id == c_.id) continue;
-
-        int dist = (c.position - c_.position + N) % N;
-        if (dist > 0 && dist < best_dist) {
-            best_dist = dist;
-            best_id = c.id;
-        }
-    }
-    return best_id;
-}
-
-bool vacant_next_l(std::vector<Car>& cars, const Car& c_, int N) {
-    for (auto const& c : cars) {
-        if (c.lane == c_.lane) continue;
-        if (c.position == c_.position) return false;
-    }
-    return true;
-}
-
-bool can_switch(std::vector<Car>& cars, const Car& c_, int N) {
-    auto c0 = cars[c0_id(cars, c_, N)];
-    auto c2 = cars[c2_id(cars, c_, N)];
-    auto c3 = cars[c3_id(cars, c_, N)];
-    int d0 = c_.position - c0.position;
-    int d2 = c2.position - c_.position;
-    int d3 = c3.position - c_.position;
-    return d2 < d3 && c_.v >= d2 && d0 > c0.v && vacant_next_l(cars, c_, N);
-}
-
-bool is_moving(const Car& c) {
-    return c.v != 0;
-}
-
-bool can_move(std::vector<Car>& cars, const Car& c_, int N) {
-    auto c2 = cars[c2_id(cars, c_, N)];
-    int d2 = c2.position - c_.position;
-    return c_.v == 0 && d2 > 1;
-}
-
-bool acc_p(std::vector<Car>& cars, Car c_, int N, double ss, const double& p, const int& vmax) {
-    if (ss == 1.0 || ss >= p) {
-        auto c2 = cars[c2_id(cars, c_, N)];
-        int d2 = c2.position - c_.position;
-        c_.v = std::min(d2 - 1, std::min(c_.v + 1, vmax));
-        return true;
-    }
-    return false;
-}
-
-bool deterministic_dec(std::vector<Car>& cars, Car c_, int N) {
-    auto c2 = cars[c2_id(cars, c_, N)];
-    int d2 = c2.position - c_.position;
-    if (d2 <= c_.v && (c_.v < c2.v || c_.v < 2)) {
-        c_.v = d2 - 1;
-        return true;
-    }
-    if (d2 <= c_.v && c_.v >= c2.v && c_.v >= 2) {
-        c_.v = std::min(d2 - 1, c_.v - 2);
-        return true;
-    }
-    if (c_.v < d2 && d2 <= 2 * c_.v && c_.v >= c2.v) {
-        c_.v = c_.v - std::floor((c_.v - c2.v)/2.0);
-        return true;
-    }
-    return false;
-}
-
-void deterministic_dec_acc(std::vector<Car>& cars, Car c_, int N) {
-    if (!deterministic_dec) {
-
-    }
-}
-
-
-void single_update(std::vector<Car>& cars, Car c, int N, const int& vmax,
-    const double& p_start, double ss, 
-    const double& p_dec, double dec) {
-    
-    if (can_move(cars, c, N)) {
-        
-    }
-}
